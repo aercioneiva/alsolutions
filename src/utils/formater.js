3e91f86ca831b57209fceb773e811bc6a2b99300
Aercio Neiva <aercio@rbxsoft.com>
2026-03-16 19:50:29 -0300

    ajj

--- src/utils/formater.js
+++ src/utils/formater.js
@@ -1,7 +1,7 @@
 function formatarMoeda(valor) {
   return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL"
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
   }).format(Number(valor));
 }
 
